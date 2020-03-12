f889401b29db71ac7a5eff02154feff8a9e733ba
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-12 05:34:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
-   def set_the_name=(dog_name)
-     @the_name_variable_is_local_to_class = dog_name
+   def set_the_name=(Fido)
+     @the_name_variable_is_local_to_class = Fido
    end
 
    def get_the_name
